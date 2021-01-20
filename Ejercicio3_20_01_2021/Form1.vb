bf8d1492efb4763b8d3653e937ce3e2335ce4e98
GonWick <gonxza@gmail.com>
2021-01-20 13:37:12 +0100

    Agregar archivos de proyecto.

---
+++ Ejercicio3_20_01_2021/Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Dim M, Num1, Num2, Num3 As Double
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = ""
+        TextBox2.Text = ""
+        TextBox3.Text = ""
+
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.StartPosition = FormStartPosition.CenterScreen
+        Me.FormBorderStyle = FormBorderStyle.FixedSingle
+
+        Me.Text = "Ejercicio Precio - 20-01-2021"
+        Label1.Text = "Introduzca el precio del producto en el establecimineto número 1 , en €"
+        Label2.Text = "Introduzca el precio del producto en el establecimineto número 2 , en €"
+        Label3.Text = "Introduzca el precio del producto en el establecimineto número 3 , en €"
+        Label4.Text = ""
+        Button1.Text = "Calcular"
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Num1 = (TextBox1.Text)
+        Num2 = (TextBox2.Text)
+        Num3 = (TextBox3.Text)
+        M = (Num1 + Num2 + Num3) / 3
+        Label4.Text = ("El precio medio del producto es " & M & " €")
+    End Sub
+End Class
